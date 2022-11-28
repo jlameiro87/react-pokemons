3851b29067f04f20869a27b852162b532cd658e0
JorG <jlameiro87@gmail.com>
2022-11-28 15:04:43 -0500

    Adding details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import { PokemonProvider, usePokemon } from "./store";
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Router,
+  useMatch,
+} from "@tanstack/react-location";
+
+const location = new ReactLocation();
 
 function SearchBox() {
   const { search, setSearch } = usePokemon();
@@ -17,28 +26,95 @@ const PokemonList = () => {
   return (
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-3">
       {pokemon.map((p) => (
-        <li className="col-span-1 flex flex-col text-center bg-white rounded-lg">
-          <div className="flex-1 flex flex-col p-8">
-            <img
-              className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
-              alt=""
-            />
-            <h3 className="mt-6 text-gray-900 text-sm font-medium">{p.name}</h3>
-          </div>
-        </li>
+        <Link key={p.id} to={`/pokemon/${p.id}`}>
+          <li className="col-span-1 flex flex-col text-center bg-white rounded-lg">
+            <div className="flex-1 flex flex-col p-8">
+              <img
+                className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
+                alt=""
+              />
+              <h3 className="mt-6 text-gray-900 text-sm font-medium">
+                {p.name}
+              </h3>
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );
 };
 
+const PokemonDetail = () => {
+  const {
+    params: { id },
+  } = useMatch();
+  const { pokemon } = usePokemon();
+  const pokemonData = pokemon.find((p) => p.id === +id);
+
+  if (!pokemonData) return <div>Pokemon not found</div>;
+
+  return (
+    <div className="mt-3">
+      <Link to="/">
+        <h1 className="text-2xl font-bold mb-5">&lt; Home</h1>
+      </Link>
+      <div className="grid grid-cols-2">
+        <img
+          className="w-96 h-96 flex-shrink-0 mx-auto bg-black rounded-xl"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`}
+          alt=""
+        />
+        <div className="ml-3">
+          <h2 className="text-2xl font-bold">{pokemonData.name}</h2>
+          <div className="mt-3">
+            <h3 className="text-xl font-bold">Stats</h3>
+            <ul className="mt-3">
+              {[
+                "hp",
+                "attack",
+                "defense",
+                "special_attack",
+                "special_defense",
+                "speed",
+              ].map((stat) => (
+                <li key={stat} className="grid grid-cols-2">
+                  <span className="font-bold">{stat}</span>
+                  <span>{pokemonData[stat as keyof typeof pokemonData]}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <SearchBox />
+        <PokemonList />
+      </>
+    ),
+  },
+  {
+    path: "/pokemon/:id",
+    element: <PokemonDetail />,
+  },
+];
+
 function App() {
   return (
     <PokemonProvider>
-      <div className="mx-auto max-w-3xl">
-        <SearchBox />
-        <PokemonList />
-      </div>
+      <Router location={location} routes={routes}>
+        <div className="mx-auto max-w-3xl">
+          <Outlet />
+        </div>
+      </Router>
     </PokemonProvider>
   );
 }
